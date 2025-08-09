1daee259a80b48c9908b54591e077b028aabb094
w175128296 <80136667+w175128296@users.noreply.github.com>
2025-08-09 19:12:54 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -8,13 +8,13 @@ urls = [
     'https://ip.164746.xyz', 
     'https://cf.090227.xyz', 
     'https://stock.hostmonit.com/CloudFlareYes',
-    'https://www.wetest.vip/page/cloudflare/address_v4.html'
-    'https://addressesapi.090227.xyz/ct'
-    'https://addressesapi.090227.xyz/cmcc'
-    'https://addressesapi.090227.xyz/cmcc-ipv6'
-    'https://addressesapi.090227.xyz/CloudFlareYes'
-    'https://addressesapi.090227.xyz/ip.164746.xyz'
-    'https://ipdb.api.030101.xyz/?type=bestproxy&country=true'
+    'https://www.wetest.vip/page/cloudflare/address_v4.html',
+    'https://addressesapi.090227.xyz/ct',
+    'https://addressesapi.090227.xyz/cmcc',
+    'https://addressesapi.090227.xyz/cmcc-ipv6',
+    'https://addressesapi.090227.xyz/CloudFlareYes',
+    'https://addressesapi.090227.xyz/ip.164746.xyz',
+    'https://ipdb.api.030101.xyz/?type=bestproxy&country=true',
     'https://ipdb.api.030101.xyz/?type=bestcf&country=true'
 ]
 
